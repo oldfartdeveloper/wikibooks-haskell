6297e1c9890f97d559d9fd9f16df265a6de880a3
oldfartdeveloper <scottnelsonsmith@gmail.com>
2021-06-22 12:49:06 -0700

    Implemented Foundation.addCard

--- Solitaire/Foundation.hs
+++ Solitaire/Foundation.hs
@@ -2,3 +2,17 @@ module Solitaire.Foundation where
   
 import Prelude
 
+import Solitaire.Card (Card, Suit, deck, suit)
+
+type Foundation = [[Card]]
+
+create :: Foundation
+create = replicate (1 + fromEnum(maxBound :: Suit)) []
+
+addCard :: Foundation -> Card -> Foundation
+addCard foundation card = do
+  let suitOffset = fromEnum (suit card)
+      (beforeSuits, result) = splitAt suitOffset foundation
+      thisSuit = head result
+      afterSuits = tail result
+  beforeSuits ++ [card : thisSuit] ++ afterSuits
