d3467765a4e5a7ca556c9539aee34845a2b38f94
oldfartdeveloper <scottnelsonsmith@gmail.com>
2021-06-23 13:48:04 -0700

    Added removeCard to Foundation.hs

--- Solitaire/Foundation.hs
+++ Solitaire/Foundation.hs
@@ -6,13 +6,31 @@ import Solitaire.Card (Card, Suit, deck, suit)
 
 type Foundation = [[Card]]
 
+-- Create an empty foundation
 create :: Foundation
 create = replicate (1 + fromEnum(maxBound :: Suit)) []
 
+-- Add card to apropriate suit card list
 addCard :: Foundation -> Card -> Foundation
-addCard foundation card = do
-  let suitOffset = fromEnum (suit card)
-      (beforeSuits, result) = splitAt suitOffset foundation
-      thisSuit = head result
-      afterSuits = tail result
-  beforeSuits ++ [card : thisSuit] ++ afterSuits
+addCard foundation card =
+  manageCard Add foundation card
+
+removeCard :: Foundation -> Card -> Foundation
+removeCard foundation card =
+  manageCard Remove foundation card
+  
+data CardAction
+  = Add
+  | Remove
+
+manageCard :: CardAction -> Foundation -> Card -> Foundation
+manageCard action foundation card = do
+  let
+    suitOffset = fromEnum (suit card)
+    (beforeSuits, result) = splitAt suitOffset foundation
+    thisSuit = head result
+    afterSuits = tail result
+  case action of
+    Add -> beforeSuits ++ [card : thisSuit] ++ afterSuits
+    Remove -> beforeSuits ++ [ tail thisSuit] ++ afterSuits
+   
